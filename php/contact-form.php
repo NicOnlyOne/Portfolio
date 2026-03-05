8ea7fd6efb4857e092515d5e55bf668f24b9a08b
Nicolas-V-NicOnlyOne <197065016+NicOnlyOne@users.noreply.github.com>
2026-03-05 17:26:53 +0100

    Initial upload
    This is the first addition/upload from the existing online  website

---
+++ php/contact-form.php
@@ -0,0 +1,20 @@
+<?php
+
+	$name = trim($_POST['name']);
+	$email = trim($_POST['email']);
+	$subject = trim($_POST['subject']);
+	$message = trim($_POST['message']);
+	
+	$emailTo = 'nicolas.vollerin@niconlyone.com'; //Put your own email address here
+	if (empty($subject)) {
+	    $subject = 'Message from your niconlyone.com.';
+	}
+	$body = "Name: $name \n\nEmail: $email \n\nMessage:\n$message";
+	$headers = 'From: '.$email."\r\n" .
+        'Reply-To: '.$email."\r\n";
+
+	mail($emailTo, $subject, $body, $headers);
+	$emailSent = true;
+	echo ('SEND');
+	
+?>
